f319fa27e20b476a46ef9da46752cd9fba850c0d
Aldric-Choi-s3761165 <s3761165@student.rmit.edu.au>
2021-04-24 13:56:03 +1000

    add player.h

---
+++ Player.h
@@ -0,0 +1,31 @@
+#ifndef PLAYER_H
+#define PLAYER_H 
+
+#include <iostream>
+#include <string>
+
+class Player{
+private:
+    int id;
+    std::string name;
+    std::string email;
+    int score;
+    LinkedList* hand;
+
+public:
+    Player(int id, std::string name, std::string email);
+    Player(std::string name, LinkedList* hand);
+    ~Player();
+
+    std::string getName();
+    int getScore();
+    LinkedList* getPlayerhand();
+
+    void setPlayerName(std::string name);
+    void setPlayerScore(int score);
+
+    void displayTileHand();
+    void clearPlayerHand();
+}
+
+#endif //  PLAYER_H
