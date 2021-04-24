439f828244b2130b58ac68433a9ec4d4d998d9f1
JoshuaBesei <s3839264@student.rmit.edu.au>
2021-04-24 14:18:41 +1000

    Edited GameEngine.h

--- GameEngine.h
+++ GameEngine.h
@@ -1,16 +1,23 @@
 #ifndef GAMEENGINE.H
 #define GAMEENGINE.H 
 
+#include <string>
 #include "LinkedList.h"
+#include "Player.h"
 
 class GameEngine{
 
 public:
     GameEngine();
-    GameEngine(GameEngine& other);
     ~GameEngine();
+    bool newPlayer(std::string n);
+    bool existingPlayer(std::string n, int s, std::string h);
+    void createBoard(int row, int col, std::string states);
+    void gameRun(std::string currentPlayer);
+
 private:
-    LinkedList bag;
+    LinkedList* bag;
+    Player* players[];
     
 };
 
